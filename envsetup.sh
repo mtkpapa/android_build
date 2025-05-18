454cd1fb3b6120e98a98a088d7e1d06c8e449fe3
NurKeinNeid <nurkeinneid@derpfest.org>
2025-05-18 19:36:46 +0200

    envsetup: Increase max cache size
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1229,7 +1229,7 @@ function setup_ccache() {
             [ -z "${CCACHE_DIR}" ] && export CCACHE_DIR="$HOME/.ccache"
             echo "ccache directory found, CCACHE_DIR set to: $CCACHE_DIR" >&2
 
-            CCACHE_MAXSIZE="${CCACHE_MAXSIZE:-40G}"
+            CCACHE_MAXSIZE="${CCACHE_MAXSIZE:-45G}"
             DIRECT_MODE="${DIRECT_MODE:-false}"
 
             $CCACHE_EXEC -o compression=true -o direct_mode="${DIRECT_MODE}" -M "${CCACHE_MAXSIZE}" \
